417f60f02da9108b3382e56481eb0c7f1cc41e64
Sam <osamwonyi_eke@protonmail.com>
2026-02-10 17:03:55 -0800

    Changed to base16 tomorrow night theme

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -74,8 +74,25 @@
 
       vim.theme = {
         enable = true;
-        name = "oxocarbon";
-        style = "dark";
+        name = "base16";
+        base16-colors = {
+          base00 = "#000000";
+          base01 = "#2a2a2a";
+          base02 = "#424242";
+          base03 = "#969896";
+          base04 = "#b4b7b4";
+          base05 = "#eaeaea";
+          base06 = "#e0e0e0";
+          base07 = "#ffffff";
+          base08 = "#d54e53";
+          base09 = "#e78c45";
+          base0A = "#e7c547";
+          base0B = "#b9ca4a";
+          base0C = "#70c0b1";
+          base0D = "#7aa6da";
+          base0E = "#c397d8";
+          base0F = "#a3685a";
+        };
       };
       vim.statusline.lualine = {
         enable = true;
